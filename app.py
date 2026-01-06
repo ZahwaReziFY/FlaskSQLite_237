5626737ebbcac2621871160da62cab3d09008af3
ZahwaReziFY <zahwarezi416@gmail.com>
2026-01-06 10:57:55 +0700

    ubah lg

--- app.py
+++ app.py
@@ -5,18 +5,17 @@ app = Flask(__name__)
 DB_NAME = "books.db"
 
 def connectdb():
-    conn = sqlite3.connect (DB_NAME)
+    conn = sqlite3.connect(DB_NAME)
     conn.row_factory = sqlite3.Row
     return conn
-
 
 def init_db():
     conn = connectdb()
     conn.execute('''CREATE TABLE IF NOT EXISTS books (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        judul VARCHAR(100) NOT NULL,
-                        penulis VARCHAR(100) NOT NULL
-                    );''')
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        judul VARCHAR(100) NOT NULL,
+        penulis VARCHAR(100) NOT NULL
+    )''')
     conn.commit()
     conn.close()
 
@@ -25,9 +24,9 @@ def index():
     conn = connectdb()
     books = conn.execute("SELECT * FROM books").fetchall()
     conn.close()
-    return render_template('index.html', books=books)
+    return render_template("index.html", books=books)
 
-@app.route('/add', methods=[ 'GET', 'POST'])
+@app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
         judul = request.form['judul']
@@ -37,7 +36,7 @@ def add():
         conn.commit()
         conn.close()
         return redirect(url_for('index'))
-    return render_template('add.html')
+    return render_template("add.html")
 
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
@@ -45,7 +44,7 @@ def edit(id):
     book = conn.execute("SELECT * FROM books WHERE id = ?", (id,)).fetchone()
     if not book:
         return "Buku tidak ditemukan", 404
-    
+
     if request.method == 'POST':
         judul = request.form['judul']
         penulis = request.form['penulis']
@@ -53,9 +52,9 @@ def edit(id):
         conn.commit()
         conn.close()
         return redirect(url_for('index'))
-    
+
     conn.close()
-    return render_template('edit.html', book=book)
+    return render_template("edit.html", book=book)
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -65,6 +64,6 @@ def delete(id):
     conn.close()
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     init_db()
-    app.run(host='0.0.0.0', port=6001, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
